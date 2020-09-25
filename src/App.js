f0afba4212efd9d09ce2f8d07f40479f8651c02e
R-M-G <ryan.gregory@ryanmgregory.com>
2020-09-24 21:20:31 -0500

    set state to api and sourced in AXIOS and api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    paras: 4,
+    html: true,
+    text: '',
+  };
+
+  componentDidMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios
+      .get(
+        'http://hipsterjesus.com/api?paras=' +
+          this.state.paras +
+          '&html=' +
+          this.state.html
+      )
+      .then((response) => {
+        this.setState({ text: response.data.text }, function () {
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log("Error", err)
+      })
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <h1>Test Class</h1>
-        <h2></h2>
       </div>
     );
   }

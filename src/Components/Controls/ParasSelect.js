82603fcdef07697f1868aad8c2afd880ac704549
R-M-G <ryan.gregory@ryanmgregory.com>
2020-09-25 01:30:01 -0500

    added 3 thru 5 paragraphs as a selector

--- src/Components/Controls/ParasSelect.js
+++ src/Components/Controls/ParasSelect.js
@@ -17,6 +17,9 @@ class ParasSelect extends Component {
         <select className="form-control" onChange={this.onChange.bind(this)}>
           <option value="1">1</option>
           <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
         </select>
       </div>
     );
